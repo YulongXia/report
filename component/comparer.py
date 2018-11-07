9b1af042a21f5d2cfeb1293316d03608a9521082
xiayulong <xiayulong@hual.ai>
2018-11-07 17:11:03 +0800

    develop

--- component/comparer.py
+++ component/comparer.py
@@ -6,21 +6,40 @@ import pandas as pd
 import json
 
 from data_management.databroker.databroker import databroker
+from data_management.accessdb.accessRDB import accessRDB
+from report.component.gentblunsolvedquery import gentblunsolvedquery
 from . import abstract
 
 class comparer(abstract.abstract):
     def __init__(self,**kwargs):
         self.comparer = kwargs["comparer"]
-        self.output_file_basic_statistic = os.path.join(kwargs["output_dir_basic_statistic"],"{}-{}-{}.xlsx".format("basic",kwargs["project"],time.strftime('%Y-%m-%d-%H_%M_%S',time.localtime(time.time()))))
+        cur_time = time.strftime('%Y-%m-%d-%H_%M_%S',time.localtime(time.time()))
+        self.output_file_basic_statistic = os.path.join(kwargs["output_dir_basic_statistic"],"{}-{}-{}.xlsx".format("basic",kwargs["project"],cur_time))
+        self.output_dir_assitant_file = os.path.join(kwargs["output_dir_assitant_file"],"{}-{}-{}.xlsx".format("assitant",kwargs["project"],cur_time))
         self.ComparerDict = {"default":self.compare_default}
+        self.cursor = gentblunsolvedquery(**kwargs)
 
     def process(self,info): 
         func = self.getComparer(self.comparer)
         c1 = json.loads(info.get("batchprocessing",""))
         c2 = info.get("genstdanslib","")
         result = func(c1,c2)
+        info[self.__class__.__name__] = result
+        self.to_excel(result,self.output_dir_assitant_file)
         return result
     
+    def to_excel(self,data,output):
+        tags = ["query"].extend([tag["key"] for tag in data[0]["tags"]]).extend([key for key in data[0]["extra_tags"].keys()])
+        result = { tag:[] for tag in tags }
+        for d in data:
+            result["query"].append(d["query"])
+            for tag in d["tags"]:
+                result[tag["key"]].append(tag["value"])
+            for key,value in d["extra_tags"].items():
+                result[key].append(value)
+        df = pd.DataFrame(result)
+        df.to_excel(output,index=False)
+
     def getComparer(self,label):
         return self.ComparerDict.get(label)
     
@@ -31,29 +50,32 @@ class comparer(abstract.abstract):
         for ele in c1:
             d = c2.get(ele["query"],None)
             data["query"].append(ele["query"])
+            ele["extra_tags"] = dict()
+            ele["extra_tags"]["sim_query"] = ""
             if d is not None:
                 data["inStdAns"].append(1)
-                ele["inStdAns"] = 1
+                ele["extra_tags"]["inStdAns"] = 1
                 if len(d["result"]) == 0 or d["result"] == "Null":
                     if [ e["value"] for e in ele["tags"] if e["key_name"] == "entity" ][0] == d["entity"] and [ e["value"] for e in ele["tags"] if e["key_name"] == "intent" ][0] == d["intent"]:
                         data["compare"].append(1)
-                        ele["compare"] = 1
+                        ele["extra_tags"]["compare"] = 1
                     else:
                         data["compare"].append(0)
-                        ele["compare"] = 0
+                        ele["extra_tags"]["compare"] = 0
                 else:
                     if [ e["value"] for e in ele["tags"] if e["key_name"] == "result" ][0] == d["result"]:
                         data["compare"].append(1)
-                        ele["compare"] = 1
+                        ele["extra_tags"]["compare"] = 1
                     else:
                         data["compare"].append(0)
-                        ele["compare"] = 0
+                        ele["extra_tags"]["compare"] = 0
             else:
-                ele["inStdAns"] = 0
-                ele["compare"] = ""
+                ele["extra_tags"]["inStdAns"] = 0
+                ele["extra_tags"]["compare"] = ""
                 data["inStdAns"].append(0)
                 data["compare"].append("")
-        grouped = self.statistic(data,["inStdAns","compare"])
+                ele["extra_tags"]["sim_query"] = self.get_extra_info_from_unsolved_query_set("pquery",ele)
+        self.statistic(data,["inStdAns","compare"])
         return c1
     
     def statistic(self,data,groupby_keys):
@@ -62,5 +84,22 @@ class comparer(abstract.abstract):
         count = grouped.agg(["count"])
         count.to_excel(self.output_file_basic_statistic)
         return grouped
+    
+    def get_extra_info_from_unsolved_query_set(self,tag,data):
+        tpl_aggregate = """select value,group_concat(corpus_id) as corpus_ids from tbl_unsolved_query where key_name = {tag} group by value"""
+        ret = "Null"
+        a = accessRDB()
+        result = self.cursor.execute(stmt=tpl_aggregate.format(tag=tag))
+        target = [ t for t in data["tags"] if t["key"] == tag ]
+        if len(target) == 0:
+            return ret
+        for res in result:
+            if res["value"] == target[0]["value"]:                    
+                tmp = [ row["query"] for row in a.execute(stmt="""select query from tbl_corpus where id in (ids)""".format(ids=res["corpus_ids"])) ]
+                ret = "\n---\n".join(tmp)
+        return ret
+
+
+
         
 
