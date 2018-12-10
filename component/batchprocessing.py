979179b4e945f676d922fcc6e25f1a76fc12f715
xiayulong <xiayulong@hual.ai>
2018-12-10 14:56:30 +0800

    add process status

--- component/batchprocessing.py
+++ component/batchprocessing.py
@@ -13,6 +13,8 @@ class batchprocessing(abstract.abstract):
     sql_raw_corpus = {"taixingxiao":"""select corpus_id,desc_id,sn from tbl_tag_taixingxiao where desc_id like "taixingxiao.%.1" group by corpus_id,desc_id,sn """}
     tpl_corpus_id = """select id from tbl_corpus where query = "{}"  """
     tpl_corpus_time = {"taixingxiao":""" select time from tbl_tag_taixingxiao where corpus_id = "{corpus_id}" and desc_id = "{desc_id}" and sn = "{sn}" limit 1"""}
+
+    tpl_latest_tag = {"taixingxiao":"""select b.key_name as key_name ,a.value as value from tbl_tag_taixingxiao as a ,tbl_key as b where b.key_name in ("{tag}") and a.key_id = b.id and a.corpus_id = {corpus_id} order by a.time desc limit 1"""}
     def __init__(self,**kwargs):
         self.host = kwargs["host"]
         self.port = kwargs["port"]
@@ -39,10 +41,14 @@ class batchprocessing(abstract.abstract):
             ele["extra_tags"] = dict()
             ele["extra_tags"]["times"] = 0
             ele["extra_tags"]["appearences"] = ""
+            ele["extra_tags"]["process_status"] = ""
             res = a.execute(stmt=self.tpl_corpus_id.format(ele["query"]))
             if len(res) == 0:
                 continue
             corpus_id = res[0]["id"]
+            res = a.execute(stmt=self.tpl_latest_tag[self.project].format(tag="hual_解决状态",corpus_id=corpus_id))
+            if len(res) != 0:
+                ele["extra_tags"]["process_status"] = res[0]["value"]
             search = raw_corpus.get(corpus_id,None)
             if search is None:
                 continue
@@ -50,6 +56,7 @@ class batchprocessing(abstract.abstract):
             for obj in search:
                 res = a.execute(stmt=self.tpl_corpus_time[self.project].format(**obj))
                 appearences.append(res[0]["time"])
+            appearences.sort()
             ele["extra_tags"]["times"] = len(appearences)
             ele["extra_tags"]["appearences"] = "\n".join(appearences)
 
